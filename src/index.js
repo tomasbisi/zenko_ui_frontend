76cb9754cf615d2b137f5e9df1bccdc695597a44
Jibran Kalia <jkalia@student.42.us.org>
2017-10-21 08:24:01 -0700

    Removing refrence to login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Dashboard from './Dashboard';
-import Login from './Login'
 import Info from './Components/Info'
 
 import registerServiceWorker from './registerServiceWorker';
